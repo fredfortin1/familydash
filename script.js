a32f685f122d7c5ac3446393e1db34dc598dd2f7
fredfortin1 <102866390+fredfortin1@users.noreply.github.com>
2024-12-09 22:28:11 -0500

    Update script.js
    insertion of firebase

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAASy_RNNlpPMGiFYDEaYGppMAVpUYBRL8",
+  authDomain: "db---dashboard-famille.firebaseapp.com",
+  databaseURL: "https://db---dashboard-famille-default-rtdb.firebaseio.com",
+  projectId: "db---dashboard-famille",
+  storageBucket: "db---dashboard-famille.firebasestorage.app",
+  messagingSenderId: "1042615306170",
+  appId: "1:1042615306170:web:2e17ef57dc964f6e537f83",
+  measurementId: "G-7TBK3PMXVG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 document.addEventListener('DOMContentLoaded', function () {
   initializeModules();
 });
